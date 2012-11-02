d7c114174f0c2ea231019c0bf475fb915efb6324
Trevor Strieber <trevor@strieber.org>
2012-11-02 13:43:39 -0600

    removing ruby-mode keybinding on }

--- config/bindings.el
+++ config/bindings.el
@@ -1,12 +1,11 @@
 ;; Place your bindings here.
 
-;; For example:
-;;(define-key global-map (kbd "C-+") 'text-scale-increase)
-;;(define-key global-map (kbd "C--") 'text-scale-decrease)
-
 ;; Change ruby-mode key-bindings to ew-electric-pair
 (add-hook 'ruby-mode-hook
           (lambda ()
             (define-key ruby-mode-map "{" 'ew-electric-pair)
             (define-key ruby-mode-map "[" 'ew-electric-pair)
             (define-key ruby-mode-map "(" 'ew-electric-pair)))
+
+;; Remove ruby-mode keybinding on "}" key
+(define-key ruby-mode-map "}" nil)
