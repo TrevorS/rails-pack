6465c6e25199f04e1dcf21a2b7eb34a3086b50d6
Trevor Strieber <trevor@strieber.org>
2012-12-09 13:19:03 -0500

    add Capfile file type to ruby-mode

--- config/file-types.el
+++ config/file-types.el
@@ -12,4 +12,5 @@
 (add-to-list 'auto-mode-alist '("\\.Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.Vagrantfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.js.erb" . js2-mode))
